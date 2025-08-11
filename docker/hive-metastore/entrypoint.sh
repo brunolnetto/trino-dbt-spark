ca0bced3f46d8d69825749fe548617fe15f007ae
Bruno Peixoto <brunolnetto@gmail.com>
2025-08-11 17:10:06 -0300

    refactor() Review variables and Docker content

--- docker/hive-metastore/entrypoint.sh
+++ docker/hive-metastore/entrypoint.sh
@@ -1,29 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 export HADOOP_HOME=/opt/hadoop-3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
-export JAVA_HOME=/usr/local/openjdk-8
 
-# Make sure mariadb is ready
-MAX_TRIES=8
-CURRENT_TRY=1
-SLEEP_BETWEEN_TRY=4
-until [ "$(telnet mariadb 3306 | sed -n 2p)" = "Connected to mariadb." ] || [ "$CURRENT_TRY" -gt "$MAX_TRIES" ]; do
-    echo "Waiting for mariadb server..."
-    sleep "$SLEEP_BETWEEN_TRY"
-    CURRENT_TRY=$((CURRENT_TRY + 1))
+# Wait for MySQL to be ready
+echo "Waiting for MySQL to be ready..."
+until nc -z ${MYSQL_HOST} 3306; do
+  echo "MySQL is unavailable - sleeping"
+  sleep 2
 done
+echo "MySQL is up!"
 
-if [ "$CURRENT_TRY" -gt "$MAX_TRIES" ]; then
-  echo "WARNING: Timeout when waiting for mariadb."
-fi
-
-# Check if schema exists
-/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -dbType mysql -info
-
-if [ $? -eq 1 ]; then
-  echo "Getting schema info failed. Probably not initialized. Initializing..."
+# Check and initialize schema
+echo "Checking schema..."
+if ! /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -dbType mysql -info; then
+  echo "Schema not found. Initializing..."
   /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
 fi
 
+echo "Starting Hive Metastore..."
 /opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
